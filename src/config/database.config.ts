2fd0ed4bdb719824910d9504341300dddec5f396
TP-O <letranphong20122001@gmail.com>
2021-03-20 23:55:19 +0700

    fix(database-config): assign number value for port

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,7 +2,7 @@ import { env } from 'process';
 
 export const databaseConfig = {
   host: env.DB_HOST || 'localhost',
-  port: env.DB_PORT || 3306,
+  port: parseInt(env.DB_PORT || '3306', 10),
   database: env.DB_DATABASE || 'database',
   user: env.DB_USERNAME || 'root',
   password: env.DB_PASSWORD || '',
