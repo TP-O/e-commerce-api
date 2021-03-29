197248eef6580d2130c438833b7e3fb7349b54bc
TP-O <letranphong20122001@gmail.com>
2021-03-29 16:50:36 +0700

    ci(test): install dependencies before run test

--- setup.ts
+++ setup.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 
 export const setUp = () => {
   return new Promise((resolve, reject) => {
-    exec('yarn db migrate:refresh && yarn db seed', (err, stdout) => {
+    exec('npm run db migrate:refresh && npm run db seed', (err, stdout) => {
       if (err) reject(err);
       resolve(stdout);
     });
