776eec5fc85c6de51a554784a0c5bcf01d28f642
TP-O <letranphong20122001@gmail.com>
2021-04-03 23:24:01 +0700

    refactor(controller): create account namespace

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,51 +9,45 @@ router.get('/', (_: any, res: any) => {
   });
 });
 
-router.group(
-  {
-    prefix: '/api/v1',
-  },
-  () => {
-    router.group(
-      {
-        prefix: '/users',
-      },
-      () => {
-        router.get('/', 'user.controller@all');
-        router.get('/:id', 'user.controller@index');
-        router.post('/', 'user.controller@create');
-        router.put('/:id', 'user.controller@update');
-        router.delete('/:id', 'user.controller@delete');
-      },
-    );
+router.group({ prefix: '/api/v1' }, () => {
+  /**
+   * Accounts.
+   */
+  router.group({ namespace: 'account' }, () => {
+    /**
+     * User resources.
+     */
+    router.group({ prefix: '/users' }, () => {
+      router.get('/', 'user.controller@all');
+      router.post('/', 'user.controller@create');
+      router.get('/:id', 'user.controller@index');
+      router.put('/:id', 'user.controller@update');
+      router.delete('/:id', 'user.controller@delete');
+    });
 
-    router.group(
-      {
-        prefix: '/admins',
-      },
-      () => {
-        router.get('/', 'admin.controller@all');
-        router.get('/:id', 'admin.controller@index');
-        router.post('/', 'admin.controller@create');
-        router.put('/:id', 'admin.controller@update');
-        router.delete('/:id', 'admin.controller@delete');
-      },
-    );
+    /**
+     * Admin resources.
+     */
+    router.group({ prefix: '/admins' }, () => {
+      router.get('/', 'admin.controller@all');
+      router.post('/', 'admin.controller@create');
+      router.get('/:id', 'admin.controller@index');
+      router.put('/:id', 'admin.controller@update');
+      router.delete('/:id', 'admin.controller@delete');
+    });
 
-    router.group(
-      {
-        prefix: '/salesmans',
-      },
-      () => {
-        router.get('/', 'salesman.controller@all');
-        router.get('/:id', 'salesman.controller@index');
-        router.post('/', 'salesman.controller@create');
-        router.put('/:id', 'salesman.controller@update');
-        router.delete('/:id', 'salesman.controller@delete');
-      },
-    );
-  },
-);
+    /**
+     * Salesman resources.
+     */
+    router.group({ prefix: '/salesmans' }, () => {
+      router.get('/', 'salesman.controller@all');
+      router.post('/', 'salesman.controller@create');
+      router.get('/:id', 'salesman.controller@index');
+      router.put('/:id', 'salesman.controller@update');
+      router.delete('/:id', 'salesman.controller@delete');
+    });
+  });
+});
 
 export const registerRouter = (app: Express) => {
   app.use(router.init());
