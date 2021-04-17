ac0109d4dc0fb69103c10c102b62a9c7f157ecfb
TP-O <letranphong20122001@gmail.com>
2021-04-17 18:43:52 +0700

    chore: modify test config

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,14 @@
 import { exec } from 'child_process';
 
-export const setUp = () => {
+function setUp() {
   return new Promise((resolve, reject) => {
     exec('npm run db migrate:refresh && npm run db seed', (err, stdout) => {
       if (err) reject(err);
       resolve(stdout);
     });
   });
-};
+}
+
+(async () => {
+  await setUp();
+})();
